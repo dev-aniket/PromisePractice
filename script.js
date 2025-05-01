59bbb50ec6f7587916db497bc03718c651730be9
Aniket Mallick <aniketmallick0007@gmail.com>
2025-05-02 01:24:09 +0530

    Promise Practice basics

---
+++ script.js
@@ -0,0 +1,22 @@
+function stepOne(){
+    return new Promise(function(res, rej){
+        console.log("Step 1");
+        res();
+    })
+}
+function stepTwo(){
+    return new Promise(function(res, rej){
+        console.log("Step 2");
+        res();
+    })
+}
+function stepThree(){
+    return new Promise(function(res, rej){
+        console.log("Step 3");
+        res();
+    })
+}
+
+stepOne().then(stepTwo).then(stepThree).then(function(){
+    console.log("All Task Completed");
+})
